ddd6eae232429462fa8221eb75319b61e6e612d1
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-08-27 19:22:11 +0500

    ✨ feat: add authentication

---
+++ src/middlewares/api-key-authentication.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+
+const authentication = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    return res.status(401).json({ message: "Missing authorization header" });
+  }
+
+  const apiKey = authHeader.split("Bearer ").at(-1);
+  if (!apiKey) {
+    return res.status(401).json({ message: "Invalid authorization format" });
+  }
+
+  if (process.env.API_KEY && apiKey === process.env.API_KEY) {
+    res.status(200).send("ready to go");
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+export default authentication;
--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { pingController } from "../controllers";
 import validatePingMessage from "../middlewares/ping-message-validator";
 import { rateLimit } from "express-rate-limit";
+import authentication from "../middlewares/api-key-authentication";
 
 const limiter = rateLimit({
   windowMs: 1000,
@@ -13,6 +14,7 @@ const limiter = rateLimit({
 const route = express.Router();
 
 route.use(limiter);
+route.use(authentication);
 route.post("/ping", validatePingMessage, pingController);
 
 export default route;
