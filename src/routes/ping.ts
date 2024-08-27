a8ca7b19b85642953667f4e42dc8c6328a992fb5
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-08-27 17:21:47 +0500

    ✨ feat: add rate-limiting 🚫

--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import { pingController } from "../controllers";
 import validatePingMessage from "../middlewares/ping-message-validator";
+import { rateLimit } from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 1000,
+  limit: 5,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
 
 const route = express.Router();
 
-route.use();
-
+route.use(limiter);
 route.post("/ping", validatePingMessage, pingController);
+
 export default route;
