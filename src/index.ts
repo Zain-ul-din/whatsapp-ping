7fb725b90d0ef18341b78498a653a545ae10635f
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-08-28 17:23:47 +0500

    ✨ feat: add platformsh package

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { pingRoute } from "./routes";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
+import config from "platformsh-config";
+
+config.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -17,6 +20,6 @@ app.use((req, res) => {
   res.status(404).json({ message: "Resource not found" });
 });
 
-app.listen(PORT, () => {
-  console.log(`server is listening on localhost:${PORT}`);
+app.listen(config.port || PORT, () => {
+  console.log(`server is listening on localhost:${config.port || PORT}`);
 });
