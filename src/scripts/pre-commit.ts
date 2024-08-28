e19da1c9dd5a9b2ea15a3abbdc03e905c72b8bf6
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-08-28 12:10:17 +0500

    ♻ ref: add changes to git before commit

--- src/scripts/pre-commit.ts
+++ src/scripts/pre-commit.ts
@@ -7,6 +7,7 @@ dotenv.config();
 import path from "path";
 import { encrypt } from "../lib/cipher";
 import { existsSync, readdirSync, readFileSync, writeFileSync } from "fs";
+import { exec } from "child_process";
 
 const folderPath = "/auth_info_baileys";
 const dirPath = path.join(process.cwd(), folderPath);
@@ -27,6 +28,17 @@ readdirSync(dirPath).forEach((file) => {
   } catch (_) {
     // file is already encrypted
   }
+});
+
+exec(`git add ${dirPath}`, (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+    return;
+  }
+
+  console.log(stdout);
+  console.log(stderr);
 });
 
 console.log("Success - All Auth files has been encrypted ✔");
