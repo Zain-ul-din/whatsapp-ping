28f50a14b4add0bb7f81ea562588b72bcbd0821e
Zain_ul_din <78583049+Zain-ul-din@users.noreply.github.com>
2024-09-03 09:58:51 +0000

    🔧 chore: made few changes

--- api/index.ts
+++ api/index.ts
@@ -1,22 +1,3 @@
-import express from "express";
-import { pingRoute } from "../src/routes";
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import cors from "cors";
+import startApp from "../src";
 
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-dotenv.config();
-
-app.use(cors({ origin: "*" }));
-app.use(bodyParser.json());
-app.use(pingRoute);
-
-app.use((req, res) => {
-  res.status(404).json({ message: "Resource not found" });
-});
-
-app.listen(PORT, () => {
-  console.log(`server is listening on localhost:${PORT}`);
-});
+startApp();
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import express from "express";
-import { pingRoute } from "./routes";
+import { pingRoute } from "../src/routes";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
-import config from "platformsh-config";
-
-config.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -20,6 +17,10 @@ app.use((req, res) => {
   res.status(404).json({ message: "Resource not found" });
 });
 
-app.listen(config.port || PORT, () => {
-  console.log(`server is listening on localhost:${config.port || PORT}`);
-});
+const startApp = ()=> {  
+  app.listen(PORT, () => {
+    console.log(`server is listening on localhost:${PORT}`);
+  });
+}
+
+export default startApp;
