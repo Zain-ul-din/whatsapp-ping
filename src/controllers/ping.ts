06f2438896a16f3078ece3da4f164d759820331b
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-09-08 02:47:21 +0500

    ✨ feat: add local auth_state if mongo path not provided

--- src/controllers/ping.ts
+++ src/controllers/ping.ts
@@ -1,14 +1,26 @@
 import { Request, Response } from "express";
 import { connectToWhatsApp } from "../baileys";
 import { delay } from "../lib/delay";
+import AsyncReturnType from "../types/AsyncReturnType";
+
+type WaClientType = AsyncReturnType<typeof connectToWhatsApp>;
+let waClient: WaClientType;
+
+if (process.env.SELF_HOSTED || process.env.LOCAL) {
+  (async () => {
+    waClient = await connectToWhatsApp();
+  })();
+}
 
 const pingController = async (req: Request, res: Response): Promise<void> => {
-  const waClient = await connectToWhatsApp();
+  if (!waClient) {
+    waClient = await connectToWhatsApp();
+  }
   const { message, numbers } = req.body;
   for (let number of numbers) {
     const id = `${number}@s.whatsapp.net`;
     await waClient.sendMessage(id, { text: message });
-    await delay(100);
+    await delay(parseInt(process.env.NEXT_MSG_DELAY || "100") || 100);
   }
   res.status(200).send("success");
 };
