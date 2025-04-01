28bb069eea02c58415d94a37b63f453d4502a3b1
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2025-04-01 15:50:22 +0500

    feat: include timestamp in connection status updates

--- src/services/firestore.ts
+++ src/services/firestore.ts
@@ -1,11 +1,15 @@
-import { doc, setDoc } from "firebase/firestore";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { collections } from "../lib/firebase";
 import { BaileysConnectionStatus } from "../types/BaileysConnectionStatus";
 
 export async function updateConnectionStatus(status: BaileysConnectionStatus) {
   const docRef = doc(collections.users, process.env.USER_ID);
   try {
-    await setDoc(docRef, status, { merge: true });
+    await setDoc(
+      docRef,
+      { ...status, updatedAt: serverTimestamp() },
+      { merge: true }
+    );
   } catch (err) {
     console.error(err);
   }
