c1783cfdb917457bc745d0da8350d91d235a0314
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2025-04-03 15:42:49 +0500

    refactor: streamline payload construction in saveContacts function

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,16 @@
 import { Contact } from "@whiskeysockets/baileys";
-
 const BASE_URL = process.env.API_URL ?? "";
 
 export async function saveContacts(contacts: Partial<Contact>[]) {
-  const payload = {
-    tenantId: process.env.TENANT_ID,
-    contacts: contacts
-  };
-
   await fetch(`${BASE_URL}/data/contacts`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${process.env.SECRET}`
     },
     method: "POST",
-    body: JSON.stringify(payload)
+    body: JSON.stringify({
+      tenantId: process.env.TENANT_ID,
+      contacts: contacts
+    })
   });
 }
